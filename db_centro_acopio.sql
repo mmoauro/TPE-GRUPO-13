f01754e55b7b18f2614441da3298e67bf45d82ea
ferminmedina1 <ferminmedina2002@gmail.com>
2021-06-16 16:37:02 -0300

    base de datos actualizada

--- db_centro_acopio.sql
+++ db_centro_acopio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-06-2021 a las 08:38:02
+-- Tiempo de generación: 16-06-2021 a las 21:36:20
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.6
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `db_centro_acopio`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cartonero`
+--
+
+CREATE TABLE `cartonero` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `fecha_nacimiento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cartonero`
+--
+
+INSERT INTO `cartonero` (`id`, `nombre`, `apellido`, `dni`, `direccion`, `fecha_nacimiento`) VALUES
+(0, 'vecino buena onda', '', 0, '', '0000-00-00'),
+(3, 'raul', 'medina', 2040201, 'Rebol 667', '1993-10-16');
 
 -- --------------------------------------------------------
 
@@ -45,6 +68,34 @@ CREATE TABLE `material` (
   `descripcion` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `material`
+--
+
+INSERT INTO `material` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Aluminio', ''),
+(2, 'madera', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pesaje`
+--
+
+CREATE TABLE `pesaje` (
+  `id` int(11) NOT NULL,
+  `peso` int(11) NOT NULL,
+  `id_material` int(11) NOT NULL,
+  `id_cartonero` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `pesaje`
+--
+
+INSERT INTO `pesaje` (`id`, `peso`, `id_material`, `id_cartonero`) VALUES
+(1, 10, 1, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -60,6 +111,13 @@ CREATE TABLE `solicitud_retiro` (
   `franja_horaria` varchar(15) NOT NULL,
   `volumen` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `solicitud_retiro`
+--
+
+INSERT INTO `solicitud_retiro` (`id`, `nombre`, `apellido`, `direccion`, `telefono`, `franja_horaria`, `volumen`) VALUES
+(4, 'Fermin', 'Medina', 'Colombia', 2147483647, '9 a 12hs', 'Entra en una caja');
 
 -- --------------------------------------------------------
 
@@ -81,6 +139,12 @@ CREATE TABLE `usuario` (
 --
 
 --
+-- Indices de la tabla `cartonero`
+--
+ALTER TABLE `cartonero`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `imagen_solicitud`
 --
 ALTER TABLE `imagen_solicitud`
@@ -92,6 +156,14 @@ ALTER TABLE `imagen_solicitud`
 --
 ALTER TABLE `material`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pesaje`
+--
+ALTER TABLE `pesaje`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_material` (`id_material`),
+  ADD KEY `id_cartonero` (`id_cartonero`);
 
 --
 -- Indices de la tabla `solicitud_retiro`
@@ -110,16 +182,34 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `cartonero`
+--
+ALTER TABLE `cartonero`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT de la tabla `imagen_solicitud`
 --
 ALTER TABLE `imagen_solicitud`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
+-- AUTO_INCREMENT de la tabla `material`
+--
+ALTER TABLE `material`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `pesaje`
+--
+ALTER TABLE `pesaje`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `solicitud_retiro`
 --
 ALTER TABLE `solicitud_retiro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
@@ -136,6 +226,13 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `imagen_solicitud`
   ADD CONSTRAINT `FK_SOLICITUD` FOREIGN KEY (`id_solicitud`) REFERENCES `solicitud_retiro` (`id`);
+
+--
+-- Filtros para la tabla `pesaje`
+--
+ALTER TABLE `pesaje`
+  ADD CONSTRAINT `pesaje_ibfk_1` FOREIGN KEY (`id_cartonero`) REFERENCES `cartonero` (`id`),
+  ADD CONSTRAINT `pesaje_ibfk_2` FOREIGN KEY (`id_material`) REFERENCES `material` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
